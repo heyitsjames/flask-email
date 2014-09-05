b135f4d6cecf85d1160bc23ab47b763311e04def
James Rasmussen <jamesrasmus@gmail.com>
2014-09-05 12:28:00 -0600

    added final help text for settings variables

--- uber_email/default_settings.py
+++ uber_email/default_settings.py
@@ -1,12 +1,8 @@
 DEBUG = True
 
-MANDRILL_API_KEY = '5SGhFGKPpCk7cx8qhp3j-w'
-MAILGUN_API_KEY = 'key-b6e6468eeca7f25c18f26c7beaf81ec3'
-MAILGUN_DOMAIN = 'sandbox668fc0038bf6476881a90d1ffb5e437c.mailgun.org'
-
-# MANDRILL_API_KEY = '<your mandrill key here>'
-# MAILGUN_API_KEY = '<your mailgun key here>'
-# MAILGUN_DOMAIN = '<your mailgun domain here>'
+MANDRILL_API_KEY = '<your mandrill key here>'
+MAILGUN_API_KEY = '<your mailgun key here>'
+MAILGUN_DOMAIN = '<your mailgun domain here>'
 
 DEFAULT_MAILER = 'mandrill'
 
